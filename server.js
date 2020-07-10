16b2dbc47d1bed483e7223df4a183261636fd171
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-09 17:44:58 -0700

    created appgets

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var path = require("path");
+const fs = require("fs");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+}); 
